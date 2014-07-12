913a44eb766dbed34c57d273a70043b332b8bd88
Jay Wang <jayduke15@gmail.com>
2014-07-12 03:48:39 -0700

    updated apis

--- hackathon-apis/yelp.py
+++ hackathon-apis/yelp.py
@@ -2,12 +2,18 @@ import sample
 
 def main(location, radius, category='restaurant'):
     print 'Arguments: ' + category + " | " + location
-    return sample.query_api(category, location, radius)
+    return sample.query_api(category, location, radius, False, 1)
 
 output = main('4790 Canberra Court, San Jose, CA', 8.0, 'restaurant')
 
+def verbose(location, radius, index, category='restaurant'):
+    return sample.query_api(category, location, radius, True, index)
+
+verboseOutput = verbose('4790 Canberra Court, San Jose, CA', 8.0, 2, 'restaurant')
 
 print
 print '---- Output -----'
 print output 
 print '-----------------'
+print '---- Verbose -----'
+print verboseOutput
