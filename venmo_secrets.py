5c11b64d70be8a931555bf0952385e7c0e9ac528
amni <amni2015@gmail.com>
2014-07-12 05:52:16 -0700

    add back stuff

--- venmo_secrets.py
+++ venmo_secrets.py
@@ -3,6 +3,6 @@ This file contains all the Venmo API keys as well as the secret key to the app
 """
  
  
-CLIENT_ID = 1779
-CLIENT_SECRET = "QGgWJXNXt8Fs6N6LSkNYPH8nAqMDA6Xd"
+CLIENT_ID = 1822
+CLIENT_SECRET = "49vmFaM3TLtgJv6GV6k746WSYPTjcJxJ"
 APP_SECRET="asdfasdfasdqwerqwadsfaznjkrltoiu41234132kjl;"
