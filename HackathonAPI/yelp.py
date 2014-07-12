d4e929b3f187056774f3bea8c08b3d7fbb8e6828
Jay Wang <jayduke15@gmail.com>
2014-07-12 04:13:32 -0700

    removed bad files.

--- HackathonAPI/yelp.py
+++
@@ -1,8 +0,0 @@
-import sample
-
-def main(location, radius=50, category='restaurant'):
-    print 'Arguments: ' + category + " | " + location
-    return sample.query_api(category, location, radius)
-
-#output = main('4790 Canberra Court, San Jose, CA', 8.0, 'restaurant')
-
