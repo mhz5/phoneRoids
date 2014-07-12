8537c760aed1f4ff60a01a1ae8e6a75bcfe6b6a7
Ben Schwab <bschwab@coursera.org>
2014-07-12 09:07:20 -0700

    merge

--- brain.py
+++ brain.py
@@ -13,11 +13,13 @@ YELP_STATE_TWO = "yelp_2"
 
 
 def processRequest(request, phone_number):
+	texting_user = User.objects(phone_number=str(phone_number)).first()
+	state = texting_user.brain_state
 	(app, argDict, state) = parser.parseRequest(request)
 	print 'state %s' % state
 	new_brain_state = BrainState(state=state)
 	new_brain_state.save()
-	texting_user = User.objects(phone_number=str(phone_number)).first()
+	
 	print 'hits after texting'
 	# texting_user.brain_state = new_brain_state
 	# texting_user.save()
