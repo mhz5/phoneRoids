0ead153a28108c3413af2295b96c35dbb8d89bc3
amni <amni2015@gmail.com>
2014-07-12 05:29:25 -0700

    add more prints

--- brain.py
+++ brain.py
@@ -17,8 +17,11 @@ def processRequest(request, phone_number):
 	print ('brain state')
 	texting_user = User.objects(phone_number=phone_number).first()
 	print ('texting user')
+	print texting_user
 	texting_user.brain_state = new_brain_state
+	print 'hits here'
 	texting_user.save()
+	print 'hit another'
 	query = yelp_api.query(location = argDict.get("location"), radius = argDict.get("distance", "50"), category = argDict.get("category", "restaurants"))
 	return query
 
