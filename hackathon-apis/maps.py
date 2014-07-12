11b143a5df1d048905eb186f92c118710e6e0eae
Jay Wang <jayduke15@gmail.com>
2014-07-12 00:49:59 -0700

    extracting instructions from the response.

--- hackathon-apis/maps.py
+++ hackathon-apis/maps.py
@@ -1,10 +1,15 @@
 from gmaps import Geocoding
 from gmaps import Directions
 import pprint
+import re
 
 direct = Directions()
 api = Geocoding()
 
+TAG_RE = re.compile(r'<[^>]+>')
+
+def remove_tags(text):
+    return TAG_RE.sub('', text)
 
 def getDirections(start, end):
 	direct = Directions()
@@ -12,9 +17,26 @@ def getDirections(start, end):
 	pprint.pprint(response, indent=2)
 	return response
 
-response = getDirections("4790 Canberra Court, San Jose, CA", "Hollingsworth Drive, Mountain View, CA")
-
+response = getDirections("4790 Canberra Court, San Jose, CA", "820 E El Camino Real, Mountain View, CA")
 instructionsList = response[0]['legs'][0]['steps']
 
-#for (insn in instructionsList):
+output = ''
+counter = 0
+for insn in instructionsList:
+	counter += 1
+	cur_insn = remove_tags(insn['html_instructions'])
+	cur_dist = insn['distance']['text']
+	output += str(counter) + '. ' + cur_insn + " | " + cur_dist + ' --- '
+	#print cur_insn + " | " + cur_dist
 
+
+
+
+
+
+
+
+print
+print '---- Output -----'
+print output
+print '-----------------'
