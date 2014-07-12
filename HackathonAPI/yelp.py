4fca0e154460ba085e9f683b29c925ff0e10aba1
Ben Schwab <bschwab@coursera.org>
2014-07-12 04:21:59 -0700

    speed up

--- HackathonAPI/yelp.py
+++ HackathonAPI/yelp.py
@@ -4,16 +4,9 @@ def main(location, radius, category='restaurant'):
     print 'Arguments: ' + category + " | " + location
     return sample.query_api(category, location, radius, False, 1)
 
-output = main('4790 Canberra Court, San Jose, CA', 8.0, 'restaurant')
+#output = main('4790 Canberra Court, San Jose, CA', 8.0, 'restaurant')
 
 def verbose(location, radius, index, category='restaurant'):
     return sample.query_api(category, location, radius, True, index)
 
-verboseOutput = verbose('4790 Canberra Court, San Jose, CA', 8.0, 2, 'restaurant')
-
-print
-print '---- Output -----'
-print output 
-print '-----------------'
-print '---- Verbose -----'
-print verboseOutput
+#verboseOutput = verbose('4790 Canberra Court, San Jose, CA', 8.0, 2, 'restaurant')
